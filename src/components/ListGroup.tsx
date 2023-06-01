0f605d59f399fd2a87e4c5da6e69f64eceb00109
etiiizel <franciscoetizel@gmail.com>
2023-06-01 18:04:11 -0300

    check if

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
 function ListGroup() {
-  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  items = [];
+
+  if (items.length === 0) {
+    return <p>No item found</p>;
+  }
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
-        ;
       </ul>
     </>
   );
